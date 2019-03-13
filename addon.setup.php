3207f13ea1275215f5407af9a8286464f926856e
Dennis Wyman <dennis@lanterns.io>
2019-03-13 06:38:00 -0500

    Initial commit

---
+++ addon.setup.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'name'        => 'Mailgun Forms',
+    'docs_url'    => 'https://github.com/HumphreyBoaGart/eemgforms',
+    'author'      => 'Dennis Wyman',
+    'author_url'  => 'https://denniswyman.com',
+    'version'     => '0.1.0',
+    'namespace'   => 'humphreyboagart/eemgforms',
+    'description' => 'An ExpressionEngine addon for building mail forms that use Mailgun',
+);
+
+/* End of file addon.setup.php */
+/* Location: ./system/user/addons/eemgforms/addon.setup.php */
